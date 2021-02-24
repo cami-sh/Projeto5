caf0ecbc22ce4aec669eedd72b2595b3791390b0
Camila Sayuri <camilahanazono@gmail.com>
2021-02-23 21:40:21 -0300

    Adicionado o component <Toaster />

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import { Toaster } from 'react-hot-toast'
 
 import { Provider } from 'react-redux'
 import { store } from './store'
@@ -11,6 +12,7 @@ function App() {
       <Provider store={store}>
         <Header />
       </Provider>
+      <Toaster />
     </>
   );
 }
